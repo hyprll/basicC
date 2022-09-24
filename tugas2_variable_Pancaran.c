90f39c71bd7f96163ef84e5a5b778594b39cb741
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-24 22:32:46 +0700

    a tugas 7array

--- tugas2_variable_Pancaran.c
+++ tugas2_variable_Pancaran.c
@@ -1,3 +1,4 @@
 // Write a C program to read elements in an array and find the sum of array elements.
 // C program to find sum of elements of the array.
 // How to add elements of an array using for loop in C programming. 
+
