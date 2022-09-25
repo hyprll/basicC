daecdf8c9a3fbe2c71a2e08c8ba66b7047f00215
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-25 15:29:37 +0700

    a5

---
+++ self-learn/5ConvertingType.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// pokoknya klo mau konversi tinggal tulis type apa yang mau dikonversikan
+int main(){
+    int x =10;
+    char b = 'a';
+    double c = 5.11;
+
+    int result = x + b; // bakal ngaco outputnya
+    printf("\n Hasil convertnya = %d",result);
+    
+    // DOUBLE TO INT
+    double resultD = c + x; 
+    printf("\n Hasil convertnya = %lf",resultD); 
+
+    //CONVERT DATA TYPE TO INT
+    double d = 5.67;
+    int bc = 9;
+
+    double resultT = (int)d + bc;
+    printf("\n %lf", resultT);
+
+    // value of variable convert to int 
+    double z = 5.67;
+    int y = 9;
+
+    int hasil = (int)z + y;
+    printf("%d", hasil);
+ 
+
+    return 0;
+}
