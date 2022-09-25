281dc0876cbab529e84409c072b685bd79a70d95
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-25 19:36:24 +0700

    e 10multiple case

--- self-learn/10switch.c
+++ self-learn/10switch.c
@@ -10,7 +10,7 @@
 int main(){
     int number;
     
-    printf("Enter a number for representing a month 1-4");
+    printf("\nEnter a number for representing a month 1-4");
     scanf("\n%d",&number);
 
     switch (number){
@@ -28,8 +28,34 @@ int main(){
         break;
     
     default:
-            printf("Input number between 1-4 only");
+            printf("\nInput number between 1-4 only");
         break;
     }
+
+    printf("\nSWITCH WITH MULTIPLE CASE");
+
+    int num;
+
+    printf("Enter a number between 1-7");
+    scanf("\n %d",&num);
+
+    switch (num){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        printf("Weekday");
+        break;
+    case 1:
+    case 6:
+        printf("Sunday");
+        break;
+    
+    default:
+        printf("INVALID INPUT");
+        break;
+    }
+
+
     return 0;
 }
