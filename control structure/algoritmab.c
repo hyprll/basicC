846fd9769a0fec6cf49ceb199c128fab6f240f9f
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-25 08:41:44 +0700

    m ab

--- control structure/algoritmab.c
+++ control structure/algoritmab.c
@@ -4,3 +4,22 @@
 // Display area,perimeter
 // Stop
 
+#include <stdio.h>
+int l,area,perimeter,display;
+l = 0 ;
+
+
+int main() {
+    int l[20];
+    
+    printf("Enter L number: ");
+    scanf("%s", l);
+    printf("\nL number	: %s\n", l);
+
+  
+
+	
+
+
+    return 0;
+}
