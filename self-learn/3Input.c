8835fb4a4854bf21afa42fb826dee575a013c73f
hyprll <ancaanca297@gmail.com@gmail.com>
2022-09-25 13:34:39 +0700

    e 3

--- self-learn/3Input.c
+++ self-learn/3Input.c
@@ -2,9 +2,22 @@
 
 int main(){
     int age;
+    // double input
+    double number;
+    char alphabet;
+
     printf("Enter input value: ");
     scanf("%d",&age);
-    printf("Age = %d",age);
+    printf("Age = %d\n",age);
+
+    printf("Enter double value :");
+    scanf("%lf",&number);
+    printf("Number = %d \n",number);
+
+    printf("Enter character value :");
+    scanf("\n%c",&alphabet);
+    printf("Alphabet = %c \n",alphabet);
+
 
     return 0;
 }
