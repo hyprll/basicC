3a67cd7ba33c2dba29bef86903acba7e6b69f43c
hyprll <pancaranrm13@gmail.com>
2023-01-02 20:53:50 +0700

    a parkir,parkir2,test,tugas4

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+     char password;
+     clrscr();
+     printf("Masukkan Password anda : ");
+     password=getche();
+     printf("Password anda adalah : %s", Password);
+     return0;
+   }
